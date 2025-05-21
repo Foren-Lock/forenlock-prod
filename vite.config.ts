8ced7bb8407ea0f19aac1e883c74fc35fb20c783
Foren-Lock <finalresearch11@gmail.com>
2025-05-21 13:12:34 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    server: {
+      host: "localhost", // more common than "::" (IPv6)
+      port: 8080,
+    },
+    plugins: [
+      react(),
+      ...(isDev ? [componentTagger()] : []),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
